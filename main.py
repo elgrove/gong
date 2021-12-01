f1548bc16ee2a21c75ac2828cad232ba8568c1d0
Aaron <aaron0lovegrove@gmail.com>
2021-12-01 18:57:55 +0000

    built out main

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
 from jinja2 import Template, Environment, PackageLoader, select_autoescape
 from datetime import datetime as dt
+import frontmatter
+import os
+from shutil import copytree
+import mistune
 
+md2html = mistune.Markdown()
 
 env = Environment(loader=PackageLoader("main"), autoescape=select_autoescape)
-
 template = env.get_template("index.html")
 
-posts = [
-    {
-        "title": "Title Text 1",
-        "datetime": dt.now(),
-        "content": "This is an example of a blog post with small amount of text.",
-    },
-    {
-        "title": "Title Text 2",
-        "datetime": dt.now(),
-        "content": "This is an example of a blog post with small amount of text.",
-    },
-]
+post_files = ["posts/" + f for f in os.listdir("posts")]
 
-html_template_string = template.render(posts=posts)
+posts = [frontmatter.load(p) for p in post_files]
 
-print(html_template_string)
+# cycle through posts and create list of dicts called posts
+
+
+posts_web = []
+
+
+for f, p in zip(post_files, posts):
+    d = dict(
+        date=f[6:16],  # take date from filename
+        template=p.metadata["template"],
+        title=p.metadata["title"],
+        subtitle=p.metadata["subtitle"],
+        content=md2html(p.content),
+    )
+    posts_web.append(d)
+
+
+html_template_string = template.render(posts=posts_web)
+
+
+with open("build/index.html", "w") as f:
+    f.write(html_template_string)
+
+os.system("cp -r static/ build/")
